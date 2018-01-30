81312d9aaaefb9df4c541cd47bb78daf63a506bc
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2018-01-30 13:27:07 -1000

    Add arrow function wrapper to utils

--- src/util.js
+++ src/util.js
@@ -3,15 +3,15 @@ const forNodesBySelector = (selector, f) =>
   Array.from(document.querySelectorAll(selector)).forEach(f)
 
 // Add a class name to each node.
-export const addClassNameBySelector = (selector, className) =>
+export const addClassNameBySelector = (selector, className) => () =>
   forNodesBySelector(selector, node => node.classList.add(className))
 
 // Remove a class name from each node.
-export const removeClassNameBySelector = (selector, className) =>
+export const removeClassNameBySelector = (selector, className) => () =>
   forNodesBySelector(selector, node => node.classList.remove(className))
 
 // Add inline styles to each node.
-export const addInlineStylesBySelector = (selector, styles) =>
+export const addInlineStylesBySelector = (selector, styles) => () =>
   forNodesBySelector(selector, node =>
     node.setAttribute('style', `${node.style}; ${styles}`),
   )
