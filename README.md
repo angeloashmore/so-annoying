17dbed727c4c331716eb3b4f6d54ad2fa0caf0ed
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2018-01-30 10:38:17 -1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# So Annoying
+
+Create annoying behaviors for sweet conversions. This library makes it easy to
+implement conditionally activated pop-ups, call-outs, and triggers.
+
+## Status
+
+[![npm version](https://badge.fury.io/js/so-annoying.svg)](http://badge.fury.io/js/so-annoying)
+[![Build Status](https://secure.travis-ci.org/angeloashmore/so-annoying.svg?branch=master)](http://travis-ci.org/angeloashmore/so-annoying?branch=master)
+
+## Installation
+
+```sh
+npm install --save so-annoying
+```
+
+## Quick Guide
+
+### Import
+
+Import the modules.
+
+### Good luck!
+
+You're on your own. (In other words, this part hasn't been written yet.)
---
+++ rollup.config.js
@@ -0,0 +1,49 @@
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import babel from 'rollup-plugin-babel'
+import pkg from './package.json'
+
+export default [
+  // Browser-friendly UMD build.
+  {
+    input: './src/index.js',
+    output: {
+      file: pkg.browser,
+      format: 'umd',
+      name: pkg.name,
+      sourcemap: true,
+    },
+    plugins: [
+      resolve(),
+      commonjs({
+        namedExports: {
+          'node_modules/universal-cookie/lib/index.js': [
+            'Cookies',
+          ],
+        },
+      }),
+      babel(),
+    ],
+  },
+
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  {
+    input: './src/index.js',
+    external: [
+      'universal-cookie',
+    ],
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: pkg.module,
+        format: 'es',
+        sourcemap: true,
+      },
+    ],
+    plugins: [babel()],
+  },
+]
