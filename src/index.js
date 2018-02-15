ee41a774d0abd1c78f9e4f8c3822d79da82ed46c
Angelo Ashmore <angelo.ashmore@walltowall.com>
2018-02-14 16:09:51 -1000

    Add invariant and Promise support

--- src/index.js
+++ src/index.js
@@ -1,35 +1,45 @@
-import * as Cookie from 'es-cookie'
+import { set, get } from 'es-cookie'
+import invariant from 'invariant'
 
-const COOKIE_DEFAULT_KEY = '___so-annoying-global'
 const DEFAULT_OPTIONS = {
-  // Cookie will persist for at most 14 days.
-  maxAge: 60 * 60 * 24 * 14,
-
   // Never annoy if the cookie is present.
   shouldAnnoyIfCookiePresent: () => false,
+
+  // Cookie will persist for at most 14 days.
+  maxAge: 60 * 60 * 24 * 14,
 }
 
 export const annoy = (
-  key = COOKIE_DEFAULT_KEY,
+  key,
   annoyance = () => {},
   passedOptions = {},
 ) => {
+  invariant(key, 'No key received.')
+  invariant(typeof key === 'string', 'key must be a string')
+
   const { shouldAnnoyIfCookiePresent, ...cookieOptions } = {
     ...DEFAULT_OPTIONS,
     ...passedOptions,
   }
 
+  // Set cookieOptions.expires using maxAge.
+  if (cookieOptions.maxAge) {
+    const date = new Date()
+    date.setTime(date.getTime() + cookieOptions.maxAge * 1000)
+    cookieOptions.expires = date
+  }
+
   // Fetch the cookie.
-  const existingCookie = Cookie.get(key)
+  const existingCookie = get(key)
 
   // If the cookie does not exist OR the cookie does exists and
   // options.shouldAnnoyIfCookiePresent returns true, run the annoyance and set
   // a cookie with the provided options to mark the annoyance as ran.
   if (!existingCookie || shouldAnnoyIfCookiePresent(existingCookie)) {
     // Run the annoyance and save the return value.
-    const value = annoyance(existingCookie)
-
-    // Set the cookie to mark that the annoyance was ran.
-    Cookie.set(key, value, cookieOptions)
+    annoyance(existingCookie).then(value => {
+      // Set the cookie to mark that the annoyance was ran.
+      set(key, value, cookieOptions)
+    })
   }
 }
